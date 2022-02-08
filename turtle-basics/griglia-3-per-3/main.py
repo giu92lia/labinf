efada6c880f96746659b5cc7803a7295336559b2
giu92lia <99205047+giu92lia@users.noreply.github.com>
2022-02-08 10:59:16 +0100

    Update main.py

--- turtle-basics/griglia-3-per-3/main.py
+++ turtle-basics/griglia-3-per-3/main.py
@@ -4,16 +4,33 @@ import turtle
 
 pippo = turtle.Turtle(); # crea una tartaruga, chiamata pippo
 
-# rettangolo esterno
-pippo.forward(300);
-pippo.left(90);
-pippo.forward(300);
-pippo.left(90);
-pippo.forward(300);
-pippo.left(90);
-pippo.forward(300);
 
-# aggiungere codice qui sotto
+# rettangolo esterno (ho modificato le dimensioni della griglia perchè non riuscivo a visualizzare l'intero disegno nella finestra)
+pippo.forward(99);
+pippo.left(90);
+pippo.forward(99);
+pippo.left(90);
+pippo.forward(99);
+pippo.left(90);
+pippo.forward(99);
 
-
+#rettangolo interno
+pippo.left(90);
+pippo.forward(33);
+pippo.left(90);
+pippo.forward(99);
+pippo.right(90);
+pippo.forward(33);
+pippo.right(90);
+pippo.forward(99);
+pippo.left(90);
+pippo.forward(33);
+pippo.left(90);
+pippo.forward(33);
+pippo.left(90);
+pippo.forward(99);
+pippo.right(90);
+pippo.forward(33);
+pippo.right(90);
+pippo.forward(99);
 turtle.done()
